85ccb661493feed7622e23d71f70885362105641
Kwame Anderson <kmanderson@st.ug.edu.gh>
2023-07-23 21:29:43 +0000

    Initial commit

---
+++ consts/hotels.js
@@ -0,0 +1,37 @@
+const hotels = [
+    {
+      id: '1',
+      name: 'Pentagon',
+      location: 'Green street,Central district',
+      price: 120,
+      image: require('../assets/fiveside.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '2',
+      name: 'Commonwealth Hall',
+      location: 'Yuki street',
+      price: 70,
+      image: require('../assets/vandals.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '3',
+      name: 'Vikings Hostel',
+      location: 'Almond street',
+      price: 90,
+      image: require('../assets/vhostel.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '4',
+      name: 'Green Hotel',
+      location: 'Main street',
+      price: 100,
+      image: require('../assets/hotel4.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+  ];
+  
+  export default hotels;
+  
